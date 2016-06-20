8cd6870602c7ab1815d41cd1a3556a55f2a597db
Ayana <ayana.d.i.wilson@gmail.com>
2016-06-20 18:37:41 -0400

    registering models inside of modular mongoose models

--- index.js
+++ index.js
@@ -7,32 +7,58 @@ var InstitutionModule = require('./lib/institution');
 var TransactionModule = require('./lib/transaction'); 
 var UserModule = require('./lib/user'); 
 var AccessToken = require('./lib/accessToken');
+var mongoose = require('mongoose');
+var fs = require('fs');
+var path = require('path');
+var _ = require('lodash'); 
 
+var models =  {
+    // accounts
+    BaseAccount: AccountModule.BaseAccount,
+    BankAccount: AccountModule.BankAccount, 
+    CreditCardAccount: AccountModule.CreditCardAccount, 
+    InsuranceAccount: AccountModule.InsuranceAccount, 
+    InvestmentAccount: AccountModule.InvestmentAccount, 
+    BillAccount: AccountModule.BillAccount, 
+    LoanAccount: AccountModule.LoanAccount, 
+    PlaidAccount: PlaidAccount, 
+    // balance
+    Balance: BalanceModule, 
+    // institutions
+    BaseInstitution: InstitutionModule.BaseInstitution, 
+    PlaidInstitution: InstitutionModule.PlaidInstitution, 
+    YodleeInstitution: InstitutionModule.YodleeInstitution, 
+    // transactions 
+    BaseTransaction: TransactionModule.BaseTransaction, 
+    PlaidTransaction: TransactionModule.PlaidTransaction, 
+    YodleeTransaction: TransactionModule.YodleeTransaction,
+    // users
+    BaseUser: UserModule.BaseUser, 
+    WaitListUser: UserModule.WaitListUser,
+    User: UserModule.User, 
+    // acesstoken
+    AccessToken: AccessToken
+  }
 
-module.exports = {
-  // accounts
-  BaseAccount: AccountModule.BaseAccount,
-  BankAccount: AccountModule.BankAccount, 
-  CreditCardAccount: AccountModule.CreditCardAccount, 
-  InsuranceAccount: AccountModule.InsuranceAccount, 
-  InvestmentAccount: AccountModule.InvestmentAccount, 
-  BillAccount: AccountModule.BillAccount, 
-  LoanAccount: AccountModule.LoanAccount, 
-  PlaidAccount: PlaidAccount, 
-  // balance
-  Balance: BalanceModule, 
-  // institutions
-  BaseInstitution: InstitutionModule.BaseInstitution, 
-  PlaidInstitution: InstitutionModule.PlaidInstitution, 
-  YodleeInstitution: InstitutionModule.YodleeInstitution, 
-  // transactions 
-  BaseTransaction: TransactionModule.BaseTransaction, 
-  PlaidTransaction: TransactionModule.PlaidTransaction, 
-  YodleeTransaction: TransactionModule.YodleeTransaction,
-  // users
-  BaseUser: UserModule.BaseUser, 
-  WaitListUser: UserModule.WaitListUser,
-  User: UserModule.User, 
-  // acesstoken
-  AccessToken: AccessToken
+module.exports = function(uri, config, onComplete){
+
+  if (config){
+    _.merge(process.env, config)
+  }
+    mongoose.connect(uri, function(err){
+      if(err){ throw err; }
+
+      var modelsPath = path.join(process.cwd(), '/node_modules/@myfintech/myFinModels/lib/');
+
+      fs.readdir(modelsPath, function(err, fileList){
+        if(err) throw err;
+        fileList.forEach(function(name) {
+          if(fs.existsSync(name)){
+            require(name);
+          }
+        });
+        onComplete(models)
+      });
+    });
 }
+
